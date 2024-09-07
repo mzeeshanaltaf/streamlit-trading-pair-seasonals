b34412b008afb1eb1c5b448688b0ab88b23c4a3c
mzeeshanaltaf <154883001+mzeeshanaltaf@users.noreply.github.com>
2024-09-07 16:11:41 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,61 +1,43 @@
-import folium
-from streamlit_folium import st_folium
 import pandas as pd
 from util import *
 
-# Session state variables so that values persist across reruns
-if 'function_called' not in st.session_state:
-    st.session_state.function_called = False
-if 'latitude' not in st.session_state:
-    st.session_state.latitude = False
-if 'longitude' not in st.session_state:
-    st.session_state.longitude = False
-if 'iss_people' not in st.session_state:
-    st.session_state.iss_people = False
+page_title = "Seasonal Insights"
+page_icon = "📊"
+st.set_page_config(page_title=page_title, page_icon=page_icon, layout='centered')
+st.title(f'📊 {page_title}')
+st.write(':blue[***Empower Your Trades with Seasonal Insights***]')
+st.write(" Seasonal Insights is a powerful tool for traders, providing a comprehensive analysis of monthly seasonal "
+         "trends for various trading pairs over the last 20+ years. By identifying whether each month was bullish, "
+         "bearish, or neutral, this app helps you understand the historical biases and make informed trading "
+         "decisions. Visualize past performance and discover the monthly trends to gain an edge in the market.")
 
+# Get the list of sheets present in the Excel file
+excel_file = 'Trading_Seasonal.xlsx'
+xls = pd.ExcelFile(excel_file)
 
-# Initialize streamlit app
-page_title = "ISS Tracker"
-page_icon = "🛰️"
-st.set_page_config(page_title=page_title, page_icon=page_icon, layout="wide")
+# Get the sheet names
+sheet_names = xls.sheet_names
 
-# Application title
-st.title('International Space Station (ISS) Tracker🛰️')
-st.write(':blue[***"Tracking the ISS and its Astronauts in Real-Time! 🌌👨‍🚀***]')
-st.write("This application is your window to the International Space Station! See its real-time location as it orbits "
-         "Earth 🌍 and get an up-to-date list of the astronauts aboard 🛰️. Whether you’re a space enthusiast or just "
-         "curious, explore the wonders of space and its crew from your screen! 🌟")
+# Select box widget to choose the trading pair
+st.subheader('Trading Pair Selection:')
+trading_pair = st.selectbox('Choose the Trading Pair', sheet_names, label_visibility='collapsed')
 
-# Button for updating ISS location
-button = st.button('Update ISS Location', type='primary')
+# Read the Excel file and make the first column ('Year') as the index
+df = pd.read_excel(excel_file, sheet_name=trading_pair, index_col=0)
 
-# This logic will allow get_iss_details function to be called only when button is pressed
-if button:
-    st.session_state.function_called = False
+# Copy the last row ('Bias') into separate dataframe
+df_bias = df.tail(1)
 
-# Get the details of ISS
-iss_latitude, iss_longitude, iss_people = get_iss_details()
+# Remove the last row ('Bias') from the dataframe as it is no more needed
+df = df.drop(df.index[-1])
 
-iss_tracker_col, astronauts_name_col = st.columns(2)
-with iss_tracker_col:
-    with st.container():
-        st.subheader('ISS Location:')
+# Plot the heatmap of Seasonal Trend
+st.subheader(f'Seasonal Trend of {trading_pair} ({df.index[0]}- {df.index[-1]}):')
+plot_heatmap(df)
 
-        # Display the Map
-        map = folium.Map(location=[iss_latitude, iss_longitude], zoom_start=2)
-        # Define a custom icon for a satellite (satellite-dish)
-        icon = folium.Icon(icon='satellite-dish', color='blue', prefix='fa')
-        folium.Marker(location=[iss_latitude, iss_longitude], popup='ISS', tooltip='ISS', icon=icon).add_to(map)
-        st_data = st_folium(map, use_container_width=True)
-
-with astronauts_name_col:
-    with st.container():
-        # Display the name of Astronauts in tabular form
-        st.subheader('Name of Astronauts in ISS:')
-        df_iss_people = pd.DataFrame(iss_people, columns=['Astronauts Names'])
-        df_iss_people.index = df_iss_people.index + 1  # Start the index from 1
-        df_iss_people.index.name = 'Number'  # Update the index column name
-        st.dataframe(df_iss_people, use_container_width=True)
+# Plot the heatmap of Overall Bias
+st.subheader(f'Overall Bias of {trading_pair}:')
+plot_heatmap(df_bias)
 
 # Display footer
 display_footer()
--- util.py
+++ util.py
@@ -1,23 +1,24 @@
 import streamlit as st
-import requests
+import seaborn as sns
+import matplotlib.pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap
 
 
-# This function return the location (lang and lat) and number of astronauts in ISS
-def get_iss_details():
-    if not st.session_state.function_called:  # This condition will be True only when button is pressed
-        # Get the ISS current location and extract latitude and longitude from it
-        response = requests.get("http://api.open-notify.org/iss-now.json")
-        data = response.json()
-        st.session_state.latitude = float(data["iss_position"]["latitude"])
-        st.session_state.longitude = float(data["iss_position"]["longitude"])
+def plot_heatmap(df):
+    # Create a custom colormap
+    colors = ["red", "gray", "green"]
+    n_bins = [1, 1, 1]  # Discretize the interpolation into bins
+    cmap_name = 'custom_heatmap'
 
-        # Get the name of Astronauts in ISS
-        response = requests.get("http://api.open-notify.org/astros.json")
-        data = response.json()
-        st.session_state.iss_people = [person['name'] for person in data['people'] if person['craft'] == 'ISS']
-        st.session_state.function_called = True
+    # Create the colormap
+    cm = LinearSegmentedColormap.from_list(cmap_name, colors, N=sum(n_bins))
 
-    return st.session_state.latitude, st.session_state.longitude, st.session_state.iss_people
+    # Create the heatmap
+    plt.figure(figsize=(10, len(df) / 2))
+    sns.heatmap(df, annot=False, cmap=cm, linewidths=.5, cbar=False)
+
+    # Display the heatmap
+    st.pyplot(plt)
 
 
 def display_footer():
@@ -36,7 +37,6 @@ def display_footer():
         </style>
         <div class="footer">
             Made with ❤️ by <a href="mailto:zeeshan.altaf@92labs.ai">Zeeshan</a>.
-            Source code <a href='https://github.com/mzeeshanaltaf/streamlit-iss-tracker'>here</a>.</div> 
-        </div>
+        </div> 
     """
     st.markdown(footer, unsafe_allow_html=True)
